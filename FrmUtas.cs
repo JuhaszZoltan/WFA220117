a119aa8be336e8b42aeb11ba3e8d041d39811d03
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2022-01-17 13:46:01 +0100

    Add project files.

---
+++ FrmUtas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA220117
+{
+    public partial class FrmUtas : Form
+    {
+        public FrmUtas()
+        {
+            InitializeComponent();
+            Icon = Properties.Resources.utas;
+        }
+
+        private void FrmUtas_Load(object sender, EventArgs e)
+        {
+            string kDatum = Program.MaiDatum.ToString("yyyy-MM-01");
+            string vDatum = Program.MaiDatum.AddMonths(2).ToString("yyyy-MM-01");
+
+            using (var conn = new SqlConnection(Program.ConnectionString))
+            {
+                conn.Open();
+
+                var r = new SqlCommand(
+                    "SELECT t_kod FROM tura " +
+                    $"WHERE kezdet >= '{kDatum}' AND kezdet < '{vDatum}';",
+                    conn).ExecuteReader();
+
+                while (r.Read())
+                {
+                    cbJelentkezes.Items.Add(r[0]);
+                }
+            }
+        }
+    }
+}
